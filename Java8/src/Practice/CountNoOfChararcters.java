a33ce7b61eb14064c5bfd7510040507d597cb044
prabh <prabh@prabh>
2024-03-24 13:40:09 +0530

    Removed comment

--- Java8/src/Practice/CountNoOfChararcters.java
+++ Java8/src/Practice/CountNoOfChararcters.java
@@ -7,7 +7,6 @@ import java.util.stream.Collectors;
 public class CountNoOfChararcters {
 // program to find no of repeated character in a string
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String str = "Prabhakar"; // output = "a7b2c1d2f2f1g1"
 		String charcount = Arrays.stream(str.split(""))
 		.collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
